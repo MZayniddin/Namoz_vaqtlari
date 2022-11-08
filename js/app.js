79ef13a28d9c750be783375c8908801c1baa4b1c
M.Zayniddin <93776172+MZayniddinov@users.noreply.github.com>
2022-11-08 18:20:05 +0500

    Site was made responsive and added favicon

--- js/app.js
+++ js/app.js
@@ -1,70 +1,99 @@
-const elSelectBtn = document.querySelector('.select-icon');
-const elOptionList = document.querySelector('.options');
-const elSelectMenu = document.querySelector('.select-menu');
-const elSelected = document.querySelector('.selected');
-const elSearchInput = document.querySelector('.search');
-const elLocationCity = document.querySelector('.location-city > span');
-const elDate = document.querySelector('.year');
-const elTime = document.querySelector('.time');
-const prayTimes = document.querySelectorAll('.pray-time');
+const elSelectBtn = document.querySelector('.select-btn');
+const elOptionList = document.querySelector(".options");
+const elSelectMenu = document.querySelector(".select-menu");
+const elSelected = document.querySelector(".selected");
+const elSearchInput = document.querySelector(".search");
+const elLocationCity = document.querySelector(".location-city > span");
+const elDate = document.querySelector(".year");
+const elTime = document.querySelector(".time");
+const prayTimes = document.querySelectorAll(".pray-time");
+const elLocationIcon = document.querySelector(".location-icon");
+const elCloseIcon = document.querySelector(".close-icon");
 
-
-elSelectBtn.addEventListener('click', () => {
-  elSelectMenu.classList.toggle('active');
+elSelectBtn.addEventListener("click", () => {
+  elSelectMenu.classList.toggle("active");
   elSearchInput.focus();
-})
+});
 
-elOptionList.addEventListener('click', (event) => {
-  if(event.target.matches('li')){
+elOptionList.addEventListener("click", (event) => {
+  if (event.target.matches("li")) {
+    console.log(event.target)
+    elSelectMenu.classList.remove("active");
+    elLocationIcon.style.display = "block";
+    displayNone(elOptionList, elCloseIcon);
     elSelected.innerText = event.target.innerText;
     elLocationCity.innerText = `${event.target.innerText} shahri`;
-    elSelectMenu.classList.remove('active');
     renderPrayTimes();
   }
 });
 
-function renderList(data){
-  while(elOptionList.children[1]){
+elLocationIcon.addEventListener("click", () => {
+  elLocationIcon.style.display = "none";
+  displayBlock(elCloseIcon, elOptionList);
+  elSearchInput.focus();
+});
+
+elCloseIcon.addEventListener("click", () => {
+  elLocationIcon.style.display = "block";
+  displayNone(elCloseIcon, elOptionList);
+});
+
+
+function renderList(data) {
+  while (elOptionList.children[1]) {
     elOptionList.removeChild(elOptionList.children[1]);
   }
-  data.forEach(region => {
-    const optionEl = document.createElement('li');
-    optionEl.className = 'option';
+  data.forEach((region) => {
+    const optionEl = document.createElement("li");
+    optionEl.className = "option";
     optionEl.innerText = region;
     elOptionList.appendChild(optionEl);
-  })
-};
+  });
+}
 
 renderList(regions);
 
-elSearchInput.addEventListener('input', () => {
-  const nameRegex = new RegExp(elSearchInput.value, 'gi')
-  const searchedItem = regions.filter(item => item.match(nameRegex));
+elSearchInput.addEventListener("input", () => {
+  const nameRegex = new RegExp(elSearchInput.value, "gi");
+  const searchedItem = regions.filter((item) => item.match(nameRegex));
   renderList(searchedItem);
 });
 
-function renderPrayTimes(){
-  fetch(`https://islomapi.uz/api/present/day?region=${elSelected.innerText}`)
-  .then(response => response.json())
-  .then(data => {
-    const year = (data.date).split('-').reverse().join('-');
-    console.log(year)
-    elDate.innerText = `${data.weekday}, ${year}`
-    prayTimes.forEach((item, index) => {
-      item.innerHTML = Object.values(data.times)[index];
-    });
-  });
+function renderPrayTimes() {
+  try {
+    fetch(`https://islomapi.uz/api/present/day?region=${elSelected.innerText}`)
+      .then((response) => response.json())
+      .then((data) => {
+        const year = data.date.split("-").reverse().join("-");
+        elDate.innerText = `${data.weekday}, ${year}`;
+        prayTimes.forEach((item, index) => {
+          item.innerHTML = Object.values(data.times)[index];
+        });
+      });
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 renderPrayTimes();
 
 (() => {
-  const givePadStart = (item) => item.toString().padStart(2, '0');
+  const givePadStart = (item) => item.toString().padStart(2, "0");
   setInterval(() => {
     const time = new Date();
     const hours = time.getHours();
     const minutes = time.getMinutes();
     const seconds = time.getSeconds();
-    elTime.innerText = `${givePadStart(hours)}:${givePadStart(minutes)}:${givePadStart(seconds)}`
+    elTime.innerText = `${givePadStart(hours)}:${givePadStart(
+      minutes
+    )}:${givePadStart(seconds)}`;
   }, 1000);
 })();
+
+function displayNone(...elements){
+  elements.forEach(element => element.style.display = 'none');
+}
+
+function displayBlock(...elements){
+  elements.forEach(element => element.style.display = 'block');
+}
